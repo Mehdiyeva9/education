4c52ef31ec63ac893181348796d9ed8249394f64
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-07-15 17:26:50 +0300

    Updated

--- edu/core/models.py
+++ edu/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 class Resource(models.Model):
     title = models.CharField(max_length=50)
@@ -18,3 +19,25 @@ class HelpForm(models.Model):
     def __str__(self):
         return self.text[0:50]
     
+class SiteSettings(models.Model):
+    banner_title = models.CharField(max_length=50, blank=True, null=True)
+    banner_text = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="edu_photos/", blank=True, null=True)
+    subscribe_mail = models.EmailField(max_length=256, blank=True, null=True)
+    terms_title = models.CharField(max_length=100, blank=True, null=True)
+    terms_content = HTMLField(blank=True, null=True)
+    terms_created_at = models.DateField(auto_now_add=True, blank=True, null=True)
+    privacy_title = models.CharField(max_length=100, blank=True, null=True)
+    privacy_content = HTMLField(blank=True, null=True)
+    privacy_created_at = models.DateField(auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Site Settings'
+
+    def save(self, *args, **kwargs):
+        if not self.id and SiteSettings.objects.exists():
+            return None
+        return super(SiteSettings,self).save(*args,**kwargs)
+
+    def __str__ (self):
+        return "Setting"
